fc81646a7908d6d16393dff5fbb378a7c0293372
mds <271274119@qq.com>
2019-08-13 16:04:47 +0800

    实验样式

--- src/views/Experiment.vue
+++ src/views/Experiment.vue
@@ -2,63 +2,84 @@
     <common id="expeirment"
             :showCrumbs="false">
         <div class="expeirment-banner">
-            <div class="app-common-bg" style="">
-                <img src="../../static/images/u33.jpg">
-            </div>
             <div>
-                <div>
-                    <span>{{reportDetail.expName}}</span>
-                    <p>{{reportDetail.expIntroduct}}</p>
-                    <el-button type="warning" @click="doExperiment()">我要做实验</el-button>
-                </div>
-                <div>
-                    <video :src="reportDetail.videoUrl" controls="controls">
+                <div class="experiment-background">
+                    <span class="expeirment-title">{{reportDetail.expName}}</span>
+                    <p class="expeirment-content">{{reportDetail.expIntroduct}}</p>
+                    <el-button class="expeirment-btn" type="warning" @click="doExperiment()">我要做实验</el-button>
+                    <video :src="reportDetail.videoUrl" controls="controls" width="378" height="234" style="position: relative;left: 1100px">
                     </video>
                 </div>
             </div>
         </div>
 
-        <div style="display: block">
-            <div style="height: 200px; width: 280px; float: right; background: #00A7DB" @click="openDialog(reportDetail.data)">
-                <img src="../../static/images/u175.svg"/>
-                <span>实验资料</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #812DFF" @click="openDialog(reportDetail.require)">
-                <img src="../../static/images/u89.svg"/>
-                <span>网络要求</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #FF9000" @click="openDialog(reportDetail.descript)">
-                <img src="../../static/images/u83.svg"/>
-                <span>项目描述</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #00A7DB" @click="openDialog(reportDetail.team)">
-                <img src="../../static/images/u93.svg"/>
-                <span>项目团队</span>
-            </div>
-        </div>
-        <div style="display: block">
-            <div style="height: 200px; width: 280px; float: right; background: #157AFF" @click="openDialog(reportDetail.service)">
-                <img src="../../static/images/u87.svg"/>
-                <span>服务计划</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #30AFC2" @click="openDialog(reportDetail.guide)">
-                <img src="../../static/images/u91.svg"/>
-                <span>实验指导</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #B5C711" @click="openDialog(reportDetail.feature)">
-                <img src="../../static/images/u85.svg"/>
-                <span>项目特色</span>
-            </div>
-            <div style="height: 200px; width: 280px; float: right; background: #27C0BB" @click="openDialog(reportDetail.structure)">
-                <img src="../../static/images/u177.svg"/>
-                <span>技术架构</span>
+        <div class="home-guide">
+            <div class="containner">
+                <div class="guide-list">
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #00A7DB" @click="openDialog(reportDetail.team,'项目团队')">
+                            <img src="../../static/images/u93.svg" class="guide-img"/>
+                            <span class="guide-desc" >项目团队</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div class="guide-bg" style="height: 200px; width: 280px; float: right; background: #812DFF;" @click="openDialog(reportDetail.require,'网络要求')">
+                            <img src="../../static/images/u89.svg" class="guide-img" />
+                            <span class="guide-desc" >网络要求</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #FF9000" @click="openDialog(reportDetail.descript,'项目描述')">
+                            <img src="../../static/images/u83.svg" class="guide-img" />
+                            <span class="guide-desc">项目描述</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #00A7DB" @click="openDialog(reportDetail.data,'实验资料')">
+                            <img src="../../static/images/u175.svg"  class="guide-img"/>
+                            <span class="guide-desc" >实验资料</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
+        <div class="home-guide">
+            <div class="containner">
+                <div class="guide-list">
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #157AFF" @click="openDialog(reportDetail.service,'服务计划')">
+                            <img src="../../static/images/u87.svg" class="guide-img" />
+                            <span class="guide-desc">服务计划</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #30AFC2" @click="openDialog(reportDetail.guide,'实验指导')">
+                            <img src="../../static/images/u91.svg" class="guide-img" />
+                            <span class="guide-desc">实验指导</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #B5C711" @click="openDialog(reportDetail.feature,'项目特色')">
+                            <img src="../../static/images/u85.svg" class="guide-img" />
+                            <span class="guide-desc">项目特色</span>
+                        </div>
+                    </div>
+                    <div class="guide-item">
+                        <div style="height: 200px; width: 280px; float: right; background: #27C0BB" @click="openDialog(reportDetail.structure,'技术架构')">
+                            <img src="../../static/images/u177.svg" class="guide-img" />
+                            <span class="guide-desc">技术架构</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <el-dialog
-            title="提示"
+            title=" "
             :visible.sync="dialogVisible"
             width="800px" >
+            <div slot="title">
+                <span v-show="dialogTitle" >{{ dialogTitle }}</span>
+            </div>
             <div style="height: 480px">{{dialogContent}}</div>
         </el-dialog>
 
@@ -69,6 +90,7 @@
 import Vue from 'vue'
 import { Dialog, Button } from 'element-ui'
 
+
 export default {
     components: {
         [Dialog.name]: Dialog,
@@ -78,7 +100,8 @@ export default {
         return {
             reportDetail: {},
             dialogVisible: false,
-            dialogContent: ''
+            dialogContent: '',
+            dialogTitle: ''
         }
     },
     computed: {
@@ -102,10 +125,12 @@ export default {
                     this.reportDetail = res.data || {}
                 })
         },
-        openDialog(content) {
+        openDialog(content,title) {
             // 弹窗
             this.dialogContent = content
+            this.dialogTitle = title
             this.dialogVisible = true
+
         },
         doExperiment() {
             // 我要做实验
@@ -147,6 +172,65 @@ export default {
         .expeirment-banner {
             padding-top: 168px;
             overflow: hidden;
+        }
+        .experiment-background {
+            background-image: url(../../static/images/u33.jpg);
+            background-size: 1903px 300px;
+        }
+        .expeirment-title {
+            color: #FFFFFF;
+            font-size: 30px;
+            position: relative;
+            left:380px;top:25px;
+        }
+        .expeirment-content {
+            color: #FFFFFF;
+            font-size: 18px;
+            position: relative;
+            left:380px;top:45px;
+        }
+        .expeirment-btn {
+            color: #FFFFFF;
+            font-size: 18px;
+            position: relative;
+            left:380px;bottom:20px
+        }
+        .home-guide {
+            background: #ededed;
+            padding: 30px 0;
+            .guide-list {
+                display: flex;
+                flex-direction: row;
+                align-items: stretch;
+                .guide-item {
+                    margin-right: 20px;
+                    width: 25%;
+                    text-align: center;
+                    &:last-child {
+                        margin-right: 0;
+                    }
+                    .guide-bg {
+                        width: 123px;
+                        height: 123px;
+                        cursor: pointer;
+                        &:hover {
+                            color: @active-color;
+                        }
+                    }
+                    .guide-img {
+                        position:relative;left:45px;top:60px;
+                    }
+                    .guide-desc {
+                        color: #FFFFFF;
+                        font-size: 24px;
+                        position: relative;
+                        right:30px;top:120px;
+                        &:hover {
+                            color: @active-color;
+                        }
+                    }
+                }
+            }
         }
     }
 
